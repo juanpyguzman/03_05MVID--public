a1ddeb028dd9e2ac8a14f0708c907bafe77d870d
Ivan Fuertes <ivan.fuertes@wikitude.com>
2020-03-12 21:11:37 +0100

    AG02 Completed!!!

--- projects/AG02_TCP_Client/main.cpp
+++ projects/AG02_TCP_Client/main.cpp
@@ -1,3 +1,32 @@
-int main() {
+#include "net/socketutils.h"
+#include "net/socketaddress.h"
+#include <thread>
 
+int main() {
+  SocketUtils::init();
+
+  {
+    TCPSocketPtr socket = SocketUtils::createTCPSocket(SocketUtils::INET);
+
+    SocketAddressPtr address = SocketUtils::createIPv4FromString("127.0.0.1:9090");
+
+    socket->connectTo(*address.get());
+
+    constexpr size_t bufferLength = 255;
+    char buffer[bufferLength];
+
+    for (uint8_t i = 1; i < 10; ++i) {
+      sprintf(buffer, "%d\0", i);
+      socket->sendTo(buffer, std::string(buffer).size() + 1);
+
+      socket->receiveFrom(buffer, bufferLength);
+      std::cout << "Received = " << buffer << std::endl;
+
+      std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+
+    socket->sendTo("exit", 5 * sizeof(char));
+  }
+
+  SocketUtils::shutdown();
 }
--- projects/AG02_UDP_Client/main.cpp
+++ projects/AG02_UDP_Client/main.cpp
@@ -1,3 +1,25 @@
-int main() {
+#include "net/socketutils.h"
+#include "net/socketaddress.h"
 
+int main() {
+  SocketUtils::init();
+
+  {
+    UDPSocketPtr socket = SocketUtils::createUDPSocket(SocketUtils::INET);
+
+    SocketAddressPtr address = SocketUtils::createIPv4FromString("127.0.0.1:9090");
+
+    socket->sendTo("Hola", 5, *address.get());
+
+    constexpr size_t bufferLength = 255;
+    char buffer[bufferLength];
+
+
+    SocketAddress addressFrom;
+    int bytesReceived = socket->receiveFrom(buffer, bufferLength * sizeof(char), addressFrom);
+
+    std::cout << bytesReceived << " bytes Received, Message = " << buffer << std::endl;
+  }
+
+  SocketUtils::shutdown();
 }
