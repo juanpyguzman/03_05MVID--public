de4057bc8e7a61912f23d31b232877365c7237ef
Juan Pablo Guzmán Fernández <juanpyguzman@gmail.com>
2020-03-16 02:22:02 +0100

    Actividad 1 - Terminada

--- projects/ACT01/main.cpp
+++ projects/ACT01/main.cpp
@@ -1,10 +1,15 @@
-/* EJ01
+/*	
+	EJ01
+	El vector de la librería standard de C++ (std::vector) no es thread safe,
+	implementar una clase templatizada propia sobre std::vector que sea thread safe,
+	que implemente al menos los siguientes métodos análogos a los del propio std::vector
+	(donde T es el tipo del template que tiene que albergar nuestro vector).
+	La declaración de los métodos a implementar están en el fichero include / containers / vector.hpp.
 
-El vector de la librería standard de C++ (std::vector) no es thread safe,
-implementar una clase templatizada propia sobre std::vector que sea thread safe,
-que implemente al menos los siguientes métodos análogos a los del propio std::vector
-(donde T es el tipo del template que tiene que albergar nuestro vector).
-La declaración de los métodos a implementar están en el fichero include / containers / vector.hpp.
+	EJ02
+	Implementar un caso de uso similar al visto en la Actividad Guiada donde se haga uso de un vector
+	para provocar una condición de carrera, primero se prueba con std::vector
+	y luego con el nuevo vector se debe solucionar con éxito.
 
 */
 
@@ -15,51 +20,63 @@ La declaración de los métodos a implementar están en el fichero include / contai
 #include <thread>
 #include <mutex>
 
+Vector<uint32_t> safeVector;
+std::vector<uint32_t> unsafeVector;
 
-Vector<uint32_t> vec;
-std::vector<uint32_t> unsafevec;
+static void increaseElement(uint32_t size, bool isSafe) {
 
-void bubbleSort(Vector<uint32_t> vector, uint32_t numThreads, uint32_t thread) {
-	uint32_t temp;
-	for (uint32_t i = 1 + thread * vector.size() / numThreads; i < (1 + thread) * vector.size() / numThreads; i++)
-		for (uint32_t j = 0 + thread * vector.size() / numThreads; j < (1 + thread) * vector.size() / numThreads - 1; j++)
-			if (vector[j] > vector[j + 1]) {
-				temp = vector[j];
-				vector[j] = vector[j + 1];
-				vector[j + 1] = temp;
-			}
+	if (isSafe)
+	{
+		for (uint32_t j = 0; j < size; ++j){
+			safeVector.push_back(j);
+		}
+	}
+
+	else
+	{
+		for (uint32_t j = 0; j < size; ++j) {
+			unsafeVector.push_back(j);
+		}
+	}
 }
 
 
 int main() {
-	vec.resize(100);
-	for (int i = 0; i < vec.size(); i++)
-	{
-		vec[i] = rand() % (1000);
-		std::cout << vec[i] << std::endl;
+	std::cout << " Start" << std::endl;
+
+	bool isSafe = true;
+	
+	for (uint32_t i = 0; i < 100; ++i) {
+		std::vector<std::thread> threads;
+
+		for (uint32_t i = 0; i < 30; ++i)
+		{
+			threads.push_back(std::thread(increaseElement, 1000, isSafe));
+		}
+
+		for (auto& t : threads) {
+			t.join();
+		}
+	
+
+		if (isSafe)
+		{
+			if (safeVector.size() != 30000) {
+				std::cout << "Error in simulation " << i << ", Size of vector:  " << safeVector.size() << std::endl;
+			}
+			safeVector.clear();
+		}
+
+		else
+		{
+			if (unsafeVector.size() != 30000) {
+				std::cout << "Error in simulation " << i << ", Size of vector:  " << unsafeVector.size() << std::endl;
+			}
+			unsafeVector.clear();
+		}
 	}
 
+	std::cout << " End" << std::endl;
 
-	std::cout << std::endl << "Ordenamos" << std::endl << std::endl;
-
-
-	//std::vector<std::thread> threads;
-	Vector<std::thread> safeThreads;
-	uint32_t numThreads = 4;
-	Vector<uint32_t> temp;
-
-	for (uint32_t n = 0; n < 4; n++)
-	{
-		safeThreads[n] = std::thread(bubbleSort, &vec, numThreads, n);
-	}
-
-	for (auto& t : safeThreads) {
-		t.join();
-	}
-
-	for (int i = 0; i < vec.size(); i++)
-	{
-		std::cout << vec[i] << std::endl;
-	}
-
+	return 0;
 }
