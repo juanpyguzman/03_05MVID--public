dbd0c6895f7a07d570c54198c7bf8df25564a815
Juan Pablo Guzmán Fernández <juanpyguzman@gmail.com>
2020-03-22 23:11:01 +0100

    Función "game" creada y conexión entre server y client

--- projects/ACT02_Client/main.cpp
+++ projects/ACT02_Client/main.cpp
@@ -1,3 +1,70 @@
-int main() {
+#include "net/socketutils.h"
+#include "net/socketaddress.h"
+#include <thread>
 
+enum possibilities { end, piedra, papel, tijera, lagarto, spock};
+
+inline const char* enumToString(possibilities val)
+{
+    switch (val)
+    {
+        case piedra: return "Piedra";
+        case papel: return "Papel";
+        case tijera: return "Tijera";
+        case lagarto: return "Lagarto";
+        case spock: return "Spock";
+
+    }
+}
+
+int main() {
+    SocketUtils::init();
+
+    {
+        TCPSocketPtr socket = SocketUtils::createTCPSocket(SocketUtils::INET);
+
+        SocketAddress addressFrom;
+        SocketAddressPtr address = SocketUtils::createIPv4FromString("127.0.0.1:9090");
+
+        socket->connectTo(*address.get());
+
+        int hand;
+        int buffer;
+        bool end = false;
+
+        for (int i = 0; i < 5; ++i)
+        {
+            //Cliente saca su jugada
+            //int hand = rand() % 5;
+            std::cout << "Introduzca su jugada del 1 al 5: ";
+            std::cin >> hand;
+
+            socket->sendTo(&hand, sizeof(int));
+
+            if (hand == 0)
+            {
+                break;
+            }
+
+            socket->receiveFrom(&buffer, sizeof(int));
+
+            //Se transforma el buffer recibido en nuestras posibilidades de juego
+            possibilities valServer = static_cast<possibilities>(buffer);
+            possibilities valClient = static_cast<possibilities>(hand);
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+            std::cout << "Cliente juega: " << enumToString(valClient) << " ------ Servidor juega: " << enumToString(valServer) << std::endl;
+            if (i == 4) {
+                hand = 0;
+                socket->sendTo(&hand, sizeof(int));
+            }
+
+        }
+
+        std::cout << "Partida finalizada";
+
+    }
+
+    SocketUtils::shutdown();
 }
--- projects/ACT02_Server/main.cpp
+++ projects/ACT02_Server/main.cpp
@@ -1,3 +1,80 @@
-int main() {
+#include "net/socketutils.h"
+#include "net/socketaddress.h"
+#include "containers/vector.hpp"
+#include <thread>
+#include <vector>
 
+enum possibilities { end, piedra, papel, tijera, lagarto, spock };
+
+inline const char* enumToString(possibilities val)
+{
+    switch (val)
+    {
+    case piedra: return "Piedra";
+    case papel: return "Papel";
+    case tijera: return "Tijera";
+    case lagarto: return "Lagarto";
+    case spock: return "Spock";
+
+    }
+}
+
+void game(TCPSocketPtr& _connSocket)
+{
+    int server;
+    int client;
+
+    for (;;)
+    {
+        std::cout << "Esperando jugada desde el cliente" << std::endl;
+        _connSocket->receiveFrom(&client, sizeof(int));
+
+        if (client == 0)
+        {
+            break;
+        }
+
+        //Servidor saca su jugada
+        server = 1 + rand() % 5;
+
+        //Se transforma el buffer recibido en nuestras posibilidades de juego
+        possibilities valClient = static_cast<possibilities>(client);
+        possibilities valServer = static_cast<possibilities>(server);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+        _connSocket->sendTo(&server, sizeof(int));
+
+        std::cout << "Cliente juega: " << enumToString(valClient) << " ------ Servidor juega: " << enumToString(valServer) << std::endl;
+
+
+    }
+
+    std::cout << "Partida finalizada";
+
+}
+
+int main() {
+    SocketUtils::init();
+    //std::vector<std::thread> threadsVector;
+
+    {
+        TCPSocketPtr socket = SocketUtils::createTCPSocket(SocketUtils::INET);
+
+        SocketAddressPtr address = SocketUtils::createIPv4FromString("127.0.0.1:9090");
+
+        socket->bindTo(*address.get());
+        socket->listenTo();
+
+        SocketAddress addressIn;
+        TCPSocketPtr connSocket = socket->acceptCon(addressIn);
+       // std::thread thread = std::thread(game, connSocket);
+       // thread.detach();
+
+        game(connSocket);
+    }
+
+    SocketUtils::shutdown();
+
+    return 0;
 }
