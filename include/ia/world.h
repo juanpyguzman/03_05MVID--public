e4de0af9ec1e19937eabb906c4f97c93b3cf347d
Juan Pablo Guzmán Fernández <juanpyguzman@gmail.com>
2020-03-28 17:20:54 +0100

    Comienzo Flocking (10 agentes IA añadidos)

--- include/ia/world.h
+++ include/ia/world.h
@@ -18,21 +18,44 @@ class World {
   public:
     World() {
       target_.init(this, Body::Color::Red, Body::Type::Manual);
-      ia_.init(this, Body::Color::Green, Body::Type::Autonomous);
-      ia_.getKinematic()->position = Vec2(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+      for (uint16_t i = 0; i < (sizeof(ia_) / sizeof(*ia_)); ++i)
+      {
+          ia_[i].init(this, Body::Color::Green, Body::Type::Autonomous);
+          ia_[i].getKinematic()->position = Vec2(WINDOW_WIDTH / (2+i), WINDOW_HEIGHT / (2+i));
+      }
     };
     ~World() {
       target_.shutdown();
-      ia_.shutdown();
+      for (uint16_t i = 0; i < (sizeof(ia_) / sizeof(*ia_)); ++i)
+      {
+          ia_[i].shutdown();
+      }
     };
 
-    void update(const float dt) { target_.update(dt); ia_.update(dt); }
-    void render() const { target_.render(); ia_.render(); }
+    void update(const float dt) { 
+        target_.update(dt); 
+        for (uint16_t i = 0; i < (sizeof(ia_) / sizeof(*ia_)); ++i)
+        {
+            ia_[i].update(dt);
+        }
+    }
+    
+    void render() const {
+        target_.render();
+        for (uint16_t i = 0; i < (sizeof(ia_) / sizeof(*ia_)); ++i)
+        {
+            ia_[i].render();
+        }
+    }
+
+    uint16_t numIA() { return (sizeof(ia_) / sizeof(*ia_)); }
+
 
     Agent* target() { return &target_; }
-    Agent* ia() { return &ia_; }
+    Agent* ia(uint16_t i) { return &ia_[i]; }
   private:
-    Agent target_, ia_;
+    Agent target_;
+    Agent ia_[10]; //Definimos aquí el número de Agentes de la IA
 };
 
 #endif
--- src/ia/scene_steering.cc
+++ src/ia/scene_steering.cc
@@ -26,55 +26,94 @@ void SceneSteering::handleMouseEvent(const SDL_Event e) {}
 void SceneSteering::handleKeyEvent(const SDL_Keycode key) {
   switch (key) {
     case SDLK_1:
-      world_->ia()->setSteering(Body::SteeringMode::Kinematic_Seek);
+        for (uint16_t i = 0; i < world_->numIA(); ++i)
+        {
+            world_->ia(i)->setSteering(Body::SteeringMode::Kinematic_Seek);
+        }
       printf("Behavior Of Agent Changed To Kinematic_Seek\n");
     break;
     case SDLK_2:
-      world_->ia()->setSteering(Body::SteeringMode::Kinematic_Flee);
+        for (uint16_t i = 0; i < world_->numIA(); ++i)
+        {
+            world_->ia(i)->setSteering(Body::SteeringMode::Kinematic_Flee);
+        }
       printf("Behavior Of Agent Changed To Kinematic_Flee\n");
     break;
     case SDLK_3:
-      world_->ia()->setSteering(Body::SteeringMode::Kinematic_Arrive);
+        for (uint16_t i = 0; i < world_->numIA(); ++i)
+        {
+            world_->ia(i)->setSteering(Body::SteeringMode::Kinematic_Arrive);
+        }
       printf("Behavior Of Agent Changed To Kinematic_Arrive\n");
       break;
     case SDLK_4:
-      world_->ia()->setSteering(Body::SteeringMode::Kinematic_Wander);
+        for (uint16_t i = 0; i < world_->numIA(); ++i)
+        {
+            world_->ia(i)->setSteering(Body::SteeringMode::Kinematic_Wander);
+        }
       printf("Behavior Of Agent Changed To Kinematic_Wander\n");
       break;
     case SDLK_q:
-      world_->ia()->setSteering(Body::SteeringMode::Seek);
+        for (uint16_t i = 0; i < world_->numIA(); ++i)
+        {
+            world_->ia(i)->setSteering(Body::SteeringMode::Seek);
+        }
       printf("Behavior Of Agent Changed To Seek\n");
       break;
     case SDLK_w:
-      world_->ia()->setSteering(Body::SteeringMode::Flee);
+        for (uint16_t i = 0; i < world_->numIA(); ++i)
+        {
+            world_->ia(i)->setSteering(Body::SteeringMode::Flee);
+        }
       printf("Behavior Of Agent Changed To Flee\n");
       break;
     case SDLK_e:
-      world_->ia()->setSteering(Body::SteeringMode::Arrive);
+        for (uint16_t i = 0; i < world_->numIA(); ++i)
+        {
+            world_->ia(i)->setSteering(Body::SteeringMode::Arrive);
+        }
       printf("Behavior Of Agent Changed To Arrive\n");
       break;
     case SDLK_r:
-      world_->ia()->setSteering(Body::SteeringMode::Align);
+        for (uint16_t i = 0; i < world_->numIA(); ++i)
+        {
+            world_->ia(i)->setSteering(Body::SteeringMode::Align);
+        }
       printf("Behavior Of Agent Changed To Align\n");
       break;
     case SDLK_t:
-      world_->ia()->setSteering(Body::SteeringMode::Velocity_Matching);
+        for (uint16_t i = 0; i < world_->numIA(); ++i)
+        {
+            world_->ia(i)->setSteering(Body::SteeringMode::Velocity_Matching);
+        }
       printf("Behavior Of Agent Changed To Velocity_Matching\n");
       break;
     case SDLK_a:
-      world_->ia()->setSteering(Body::SteeringMode::Pursue);
+        for (uint16_t i = 0; i < world_->numIA(); ++i)
+        {
+            world_->ia(i)->setSteering(Body::SteeringMode::Pursue);
+        }
       printf("Behavior Of Agent Changed To Pursue\n");
       break;
     case SDLK_s:
-      world_->ia()->setSteering(Body::SteeringMode::Face);
+        for (uint16_t i = 0; i < world_->numIA(); ++i)
+        {
+            world_->ia(i)->setSteering(Body::SteeringMode::Face);
+        }
       printf("Behavior Of Agent Changed To Face\n");
       break;
     case SDLK_d:
-      world_->ia()->setSteering(Body::SteeringMode::LookGoing);
+        for (uint16_t i = 0; i < world_->numIA(); ++i)
+        {
+            world_->ia(i)->setSteering(Body::SteeringMode::LookGoing);
+        }
       printf("Behavior Of Agent Changed To LookGoing\n");
       break;
     case SDLK_f:
-      world_->ia()->setSteering(Body::SteeringMode::Wander);
+        for (uint16_t i = 0; i < world_->numIA(); ++i)
+        {
+            world_->ia(i)->setSteering(Body::SteeringMode::Wander);
+        }
       printf("Behavior Of Agent Changed To Wander\n");
       break;
     default:{}
